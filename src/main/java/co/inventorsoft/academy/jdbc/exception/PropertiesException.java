f921959fc770fb68b827b838920d6478620bae1c
appsterlight <appsterlight@gmail.com>
2023-10-26 23:20:11 +0300

    Add custom exceptions classes PropertiesException,DaoException and ServiceException

---
+++ src/main/java/co/inventorsoft/academy/jdbc/exception/PropertiesException.java
@@ -0,0 +1,7 @@
+package co.inventorsoft.academy.jdbc.exception;
+
+public class PropertiesException extends Exception {
+    public PropertiesException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/co/inventorsoft/academy/jdbc/exception/ServiceException.java
@@ -0,0 +1,14 @@
+package co.inventorsoft.academy.jdbc.exception;
+
+public class ServiceException extends Exception{
+    public ServiceException() {
+    }
+
+    public ServiceException(String message) {
+        super(message);
+    }
+
+    public ServiceException(Throwable cause) {
+        super(cause);
+    }
+}

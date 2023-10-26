165e623c87dd30f5593a8321226ba05e6bdc4a54
appsterlight <appsterlight@gmail.com>
2023-10-26 23:12:29 +0300

    Add class Field containing database field constants

---
+++ src/main/java/co/inventorsoft/academy/jdbc/dao/constants/Field.java
@@ -0,0 +1,25 @@
+package co.inventorsoft.academy.jdbc.dao.constants;
+
+public final class Field {
+
+    private Field() {    }
+    public static final String USER_ID = "id";
+    public static final String USER_FIRST_NAME = "firstname";
+    public static final String USER_LAST_NAME = "lastname";
+    public static final String USER_PHONE = "phone";
+    public static final String USER_EMAIL = "email";
+    public static final String USER_PASSWORD = "password";
+    public static final String USER_DESCRIPTION = "description";
+
+
+    public static final String COMPANY_ID = "id";
+    public static final String COMPANY_NAME = "company_name";
+    public static final String COMPANY_PHONE = "phone";
+    public static final String COMPANY_ADDRESS = "address";
+    public static final String COMPANY_CITY = "city";
+    public static final String COMPANY_REGION = "region";
+    public static final String COMPANY_ZIP = "zip";
+    public static final String COMPANY_COUNTRY = "country";
+
+
+}

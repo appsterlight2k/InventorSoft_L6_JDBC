b78c8c1c129a91ae295102fc019b777ecba47347
appsterlight <appsterlight@gmail.com>
2023-10-28 14:21:08 +0300

    Remove redundant methods from Dao parent class

--- src/main/java/co/inventorsoft/academy/jdbc/dao/CompanyDao.java
+++ src/main/java/co/inventorsoft/academy/jdbc/dao/CompanyDao.java
@@ -1,15 +1,7 @@
 package co.inventorsoft.academy.jdbc.dao;
 
-import co.inventorsoft.academy.jdbc.exception.DaoException;
 import co.inventorsoft.academy.jdbc.model.Company;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface CompanyDao extends Dao<Company> {
-    Long add(Company company) throws DaoException;
-    Optional<Company> get(Long id) throws DaoException;
-    boolean update(Company company) throws DaoException;
-    boolean delete(Long id) throws DaoException;
-    List<Company> getAll() throws DaoException;
+
 }

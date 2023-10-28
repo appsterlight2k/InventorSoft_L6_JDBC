1d03bedc866c91ab4741c9e3252be184f7cd1ee7
appsterlight <appsterlight@gmail.com>
2023-10-28 14:42:23 +0300

    Remove redundant getFieldsForStatement method and refactor setValuesInStatement to accept List instead of varargs

--- src/main/java/co/inventorsoft/academy/jdbc/dao/utils/QueryUtil.java
+++ src/main/java/co/inventorsoft/academy/jdbc/dao/utils/QueryUtil.java
@@ -1,26 +1,16 @@
 package co.inventorsoft.academy.jdbc.dao.utils;
 
-import co.inventorsoft.academy.jdbc.exception.DaoException;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Arrays;
+import java.util.List;
 
 public class QueryUtil {
     private QueryUtil() { }
 
-    public static Object[] getFieldsForStatement(Object[] allFields, boolean isUpdate) throws DaoException {
-        if (isUpdate) {
-            return allFields;
-        } else {
-            return Arrays.copyOf(allFields, allFields.length - 1);
-        }
-    }
-
     public static PreparedStatement prepareStatement(Connection connection, String query, boolean returnGeneratedKeys,
-                                                      Object... values) throws SQLException {
+                                                      List<Object> values) throws SQLException {
 
         PreparedStatement statement = connection.prepareStatement(query,
                 returnGeneratedKeys ? Statement.RETURN_GENERATED_KEYS : Statement.NO_GENERATED_KEYS);
@@ -29,11 +19,10 @@ public class QueryUtil {
         return statement;
     }
 
-    public static void setValuesInStatement(PreparedStatement statement, Object... values) throws SQLException {
-        for (int i = 0; i < values.length; i++) {
-            statement.setObject(i + 1, values[i]);
+    public static void setValuesInStatement(PreparedStatement statement, List<Object> values) throws SQLException {
+        for (int i = 0; i < values.size(); i++) {
+            statement.setObject(i + 1, values.get(i));
         }
-
     }
 
 }

26ae350067f099fc86deb5d1dd0b9ef5680abbf2
appsterlight <appsterlight@gmail.com>
2023-10-28 14:20:45 +0300

    Add findByEmail method declaration and remove redundant methods from Dao parent class

--- src/main/java/co/inventorsoft/academy/jdbc/dao/UserDao.java
+++ src/main/java/co/inventorsoft/academy/jdbc/dao/UserDao.java
@@ -3,13 +3,8 @@ package co.inventorsoft.academy.jdbc.dao;
 import co.inventorsoft.academy.jdbc.exception.DaoException;
 import co.inventorsoft.academy.jdbc.model.User;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserDao extends Dao<User> {
-    Long add(User user) throws DaoException;
-    Optional<User> get(Long id) throws DaoException;
-    boolean update(User user) throws DaoException;
-    boolean delete(Long id) throws DaoException;
-    List<User> getAll() throws DaoException;
+    Optional<User> findByEmail(String email) throws DaoException;
 }
